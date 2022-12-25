c1e8b21df93ea4fa78b8022f153c7bbeeb495c72
fedoorass <61330880+fedoorass@users.noreply.github.com>
2022-12-25 20:52:43 +0300

    Add files via upload

--- mathProblems.js
+++ mathProblems.js
@@ -4,18 +4,34 @@ const mj = function (tex) {
 
 function solve(problemcase) {
     let ansHtml = document.getElementById("answer")
-    let ans
+    let ans, ansArr
     problem = problemcase.toLowerCase()
-    console.log(problem.split("=").length - 1)
     if ((problem.split("=").length - 1) != 0) {
         ans = Algebrite.roots(problem).toString()
+        ans = ans.replace("[", "")
+        ans = ans.replace("]", "")
+        ansArr = ans.split(",")
+        ansArrFinal = []
+
+        for (let a of ansArr) {
+            ansArrFinal.push(Algebrite.simplify(a).toString())
+        }
+
+        ans="["
+        for (let a of ansArrFinal) {
+            if (a.indexOf("i") == -1) {
+                ans+=a
+                ans+=","
+            }
+        }
+        ans = ans.slice(0, -1) + ']'
+        console.log(ansArrFinal)
+        console.log(ans)
     }
     else {
         ans = Algebrite.simplify(problem).toString()
     }
-    
-    console.log(ans)
-    console.log(typeof(ans))
+
     let ansn = math.parse(ans)
     MathJax.typesetClear();
     ansHtml.innerHTML = '';
